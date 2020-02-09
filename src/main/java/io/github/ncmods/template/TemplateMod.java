552935fb7147ba21d82d0def7a942817eeaf4d1b
NCModding <60577860+ncmodding@users.noreply.github.com>
2020-02-08 17:05:23 -0700

    typo fix
    Fixes a typo that prevents Minecraft from accepting the MOD_ID as part of the String argument of an Identifier.

--- src/main/java/io/github/ncmods/template/TemplateMod.java
+++ src/main/java/io/github/ncmods/template/TemplateMod.java
@@ -7,7 +7,7 @@ package io.github.ncmods.template;
 import net.fabricmc.api.ModInitializer;
 
 public final class TemplateMod implements ModInitializer {
-	public static final String MOD_ID = "ncmods/template";
+	public static final String MOD_ID = "ncmods.template";
 
 	@Override
 	public void onInitialize() {
